e447ebf5e4de0505946883710b26516873bc05f8
yufeng <472917042@qq.com>
2021-05-27 10:42:55 +0800

    源码注释

--- src/core/util/error.js
+++ src/core/util/error.js
@@ -8,7 +8,8 @@ import { inBrowser, inWeex } from './env'
 export function handleError (err: Error, vm: any, info: string) {
   if (vm) {
     let cur = vm
-    // while是逐级寻找父级，并且调用父级的errorCaptured（如果有的话），如果父级的errorCaptured返回true的话，会阻止进一步的往上传递
+    // while是逐级寻找父级，并且调用父级的errorCaptured（如果有的话），如果父级的errorCaptured返回false的话，会阻止进一步的往上传递
+    // 选项 errorCaptured 是父组件处理子组件的错误
     while ((cur = cur.$parent)) {
       const hooks = cur.$options.errorCaptured
       if (hooks) {
@@ -26,6 +27,7 @@ export function handleError (err: Error, vm: any, info: string) {
   globalHandleError(err, vm, info)
 }
 
+// 调用全局配置的errorhandle，如果没有就往控制台抛
 function globalHandleError (err, vm, info) {
   if (config.errorHandler) {
     try {
@@ -37,6 +39,7 @@ function globalHandleError (err, vm, info) {
   logError(err, vm, info)
 }
 
+// 往控制台抛错误，会显示2种类型的错误，一种是vue自有的风格。另一个是浏览器的风格
 function logError (err, vm, info) {
   if (process.env.NODE_ENV !== 'production') {
     warn(`Error in ${info}: "${err.toString()}"`, vm)
